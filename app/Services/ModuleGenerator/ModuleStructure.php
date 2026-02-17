9e39e1b31e1cb7e28218cc179746e583a6ab9b69
yasserhegazy <yasseranahegazy@gmail.com>
2026-02-17 10:21:25 +0200

    Create ModuleStructure and TemplateFactory for file generation

---
+++ app/Services/ModuleGenerator/ModuleStructure.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services\ModuleGenerator;
+
+class ModuleStructure
+{
+    private array $directories = [
+        'Database/factories',
+        'Database/migrations',
+        'Database/seeders',
+        'Domain/DTOs',
+        'Domain/Models',
+        'Domain/Repositories',
+        'Domain/Services',
+        'Http/Controllers/Api/v1',
+        'Http/Middleware',
+        'Http/Requests',
+        'Http/Resources',
+        'Providers',
+        'Routes',
+        'Tests/Feature',
+        'Tests/Unit',
+    ];
+
+    private array $files = [
+        'config.php',
+        'helpers.php',
+        'Routes/api.php',
+    ];
+
+    public function getDirectories(): array
+    {
+        return $this->directories;
+    }
+
+    public function getFiles(): array
+    {
+        return $this->files;
+    }
+
+    public function getServiceProviderPath(string $moduleName): string
+    {
+        return "Providers/{$moduleName}ServiceProvider.php";
+    }
+}
