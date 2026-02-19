09d7c96e4b1ba3df8418dbabbed8152b3e499847
abdelrahman elshareif <abdelrahmanelshareif@gmail.com>
2026-02-19 15:54:44 +0200

    fix the comment needed

--- modules/Auth/Http/Controllers/Api/v1/RegisterController.php
+++ modules/Auth/Http/Controllers/Api/v1/RegisterController.php
@@ -4,8 +4,8 @@ namespace Modules\Auth\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Modules\Auth\Domain\Services\RegisterUserService;
+use Modules\Auth\Http\Requests\UserRegisterRequest;
 use Modules\Auth\Http\Resources\UserResource;
 
 class RegisterController extends Controller
@@ -14,9 +14,9 @@ class RegisterController extends Controller
         private RegisterUserService $registerUserService
     ) {}
 
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(UserRegisterRequest $request): JsonResponse
     {
-        $user = $this->registerUserService->handle($request->all());
+        $user = $this->registerUserService->handle($request->validated());
 
         return response()->json(
             (new UserResource($user))->resolve(),
--- modules/Auth/Routes/api.php
+++ modules/Auth/Routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\Api\v1\RegisterController;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/register', RegisterController::class);
+    Route::post('/register', RegisterController::class)->name('auth.register');
 });
