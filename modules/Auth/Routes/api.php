e1a320d718bbd8965d389aa58c1fb383a6a43cf9
abdelrahman elshareif <abdelrahmanelshareif@gmail.com>
2026-02-22 11:26:24 +0200

    add routs login/logout

--- modules/Auth/Routes/api.php
+++ modules/Auth/Routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Auth\Http\Controllers\Api\v1\LoginController;
 use Modules\Auth\Http\Controllers\Api\v1\RegisterController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', RegisterController::class)->name('auth.register');
+
+    Route::post('/login', LoginController::class)->name('auth.login');
+
+    // Protected Routes
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', \Modules\Auth\Http\Controllers\Api\v1\LogoutController::class)->name('auth.logout');
+
+        // Provided to satisfy the 'prevents access to protected routes after logout' test
+        Route::get('/me', function (Request $request) {
+            return $request->user();
+        })->name('auth.me');
+    });
 });
