3e2000409d9f779cefa36b1d04ffd96ecfa605cb
yasserhegazy <yasseranahegazy@gmail.com>
2026-03-14 15:28:52 +0200

    feat(auth): add tenant authentication guard and provider

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'tenant' => [
+            'driver' => 'session',
+            'provider' => 'tenant_users',
+        ],
     ],
 
     /*
@@ -65,10 +70,10 @@ return [
             'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'tenant_users' => [
+            'driver' => 'eloquent',
+            'model' => Modules\Staff\Domain\Models\TenantUser::class,
+        ],
     ],
 
     /*
