d2bf47f62100d0b278d7762322e98284e367d5d7
yasserhegazy <yasseranahegazy@gmail.com>
2026-03-19 12:05:37 +0200

    feat(staff): add SetupPasswordDTO and StaffLoginDTO for staff authentication

---
+++ modules/Staff/Domain/DTOs/StaffLoginDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Staff\Domain\DTOs;
+
+readonly class StaffLoginDTO
+{
+    public function __construct(
+        public string $email,
+        public string $password,
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            email: $data['email'],
+            password: $data['password'],
+        );
+    }
+}
